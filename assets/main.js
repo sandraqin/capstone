2be2f06b1ff1e0bea6cb8bcda121a8d2be277423
Sandra Qin <qinq005@newschool.edu>
2023-04-11 23:46:39 -0400

    js update

--- assets/main.js
+++ assets/main.js
@@ -27,3 +27,35 @@ window.stateCallback = () => {
   let selectElement = document.querySelector('[name="some-option"]');
   console.log(select.Element.value);
 }
+
+
+function showDiv() {
+  document.getElementById('vest').style.display = "flex";
+}
+
+function showDivshirt() {
+  document.getElementById('shirt').style.display = "flex";
+}
+
+const colorInput = document.getElementById('color-option');
+if (colorInput.value === 'Red' || colorInput.value === 'Blue' || colorInput.value === 'Black' || colorInput.value === 'Green' || colorInput.value === 'Brown') {
+  console.log('dark');
+} else {
+  console.log('light');
+}
+
+
+
+const cotton = document.getElementById('cotton');
+
+
+
+
+function showDivtip() {
+  if (parseInt(cotton.value) > 20 && colorInput.value === 'Red' || colorInput.value === 'Blue' || colorInput.value === 'Black' || colorInput.value === 'Green' || colorInput.value === 'Brown') {
+  document.getElementById('tip1').style.display = "flex";}
+  else{
+    document.getElementById('tip1').style.display = "none";
+  }
+}
+
